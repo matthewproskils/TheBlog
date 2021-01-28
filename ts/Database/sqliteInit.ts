be2e1dce68a76687cbde9c16f3d2ddfae09ffaff
Matthew <matthewproskils@gmail.com>
2021-01-27 16:24:53 -0800

    Init Commit

--- ts/Database/sqliteInit.ts
+++ ts/Database/sqliteInit.ts
@@ -2,7 +2,7 @@ import sql from "sqlite3";
 const sqlite3 = sql.verbose();
 
 function init() {
-    return sqlite3.Database('database.db')
+    return new sqlite3.Database('database.db');
 }
-
+ 
 export default init;

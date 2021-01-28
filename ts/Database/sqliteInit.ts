41e534bb135927e68b382b5bfbd83c49261897f9
Lukas <computorgenius101@gmail.com>
2021-01-27 18:05:03 -0800

    ✨ create 'blogs' table

--- ts/Database/sqliteInit.ts
+++ ts/Database/sqliteInit.ts
@@ -2,7 +2,14 @@ import sql from "sqlite3";
 const sqlite3 = sql.verbose();
 
 function init() {
-    return new sqlite3.Database('database.db');
+    const db = new sqlite3.Database('database.db');
+    db.exec(`CREATE TABLE blogs IF NOT EXISTS(
+        title TEXT
+        description TEXT
+        content TEXT
+    )`);
+
+    return db
 }
  
 export default init;

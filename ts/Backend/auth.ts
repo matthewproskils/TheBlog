e9b5e3cb6a66242c0493b939e89812d57efcc9ef
matthewproskils <matthewthechan@gmail.com>
2021-01-29 08:37:41 -0800

    Update

--- ts/Backend/auth.ts
+++ ts/Backend/auth.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 async function auth(googleUser: string) {
     const ticket = await axios.post('https://oauth2.googleapis.com/tokeninfo?id_token=' + googleUser)
 
-
     return ticket.data;
 }
 

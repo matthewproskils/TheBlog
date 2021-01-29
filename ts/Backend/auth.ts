5c535b3782c5bb1ea43ea0e7db75662dc37ba667
matthewproskils <matthewthechan@gmail.com>
2021-01-29 15:47:53 -0800

    [Insert Bad Git Commit Message]

--- ts/Backend/auth.ts
+++ ts/Backend/auth.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { google } from "googleapis";
 
 async function auth(googleUser: string) {
     const ticket = await axios.post('https://oauth2.googleapis.com/tokeninfo?id_token=' + googleUser)
